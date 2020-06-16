a2783a4cfd01601a977694213c24c62a1628234f
secret <sufay30281@gmail.com>
2020-06-16 13:32:09 +0800

    add app

---
+++ app/params/weights.go
@@ -0,0 +1,8 @@
+package params
+
+// Default simulation operation weights for messages
+const (
+	DefaultWeightMsgCreateHTLC int = 100
+	DefaultWeightMsgClaimHTLC  int = 100
+	DefaultWeightMsgRefundHTLC int = 100
+)
