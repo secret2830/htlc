77f868894f7722953d0c7f415832893d599655bc
secret <sufay30281@gmail.com>
2020-06-16 12:00:24 +0800

    add msgs test

--- types/errors.go
+++ types/errors.go
@@ -6,11 +6,11 @@ import (
 
 // HTLC module sentinel errors
 var (
-	ErrInvalidHashLock = sdkerrors.Register(ModuleName, 3, "invalid hash lock")
-	ErrInvalidTimeLock = sdkerrors.Register(ModuleName, 5, "invalid time lock")
-	ErrInvalidSecret   = sdkerrors.Register(ModuleName, 6, "invalid secret")
-	ErrHTLCExists      = sdkerrors.Register(ModuleName, 4, "htlc already exists")
-	ErrUnknownHTLC     = sdkerrors.Register(ModuleName, 4, "unknown htlc")
+	ErrInvalidHashLock = sdkerrors.Register(ModuleName, 2, "invalid hash lock")
+	ErrInvalidTimeLock = sdkerrors.Register(ModuleName, 3, "invalid time lock")
+	ErrInvalidSecret   = sdkerrors.Register(ModuleName, 4, "invalid secret")
+	ErrHTLCExists      = sdkerrors.Register(ModuleName, 5, "htlc already exists")
+	ErrUnknownHTLC     = sdkerrors.Register(ModuleName, 6, "unknown htlc")
 	ErrHTLCNotOpen     = sdkerrors.Register(ModuleName, 7, "htlc not open")
 	ErrHTLCNotExpired  = sdkerrors.Register(ModuleName, 8, "htlc not expired")
 )
